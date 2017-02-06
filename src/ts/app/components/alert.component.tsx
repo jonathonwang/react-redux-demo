98454162d970d9706c8a5a64094451b02ad1cd4b
Jonathon Kosla <jonathonkosla@gmail.com>
2017-02-06 11:04:34 -0800

    install and use classnames package

--- src/ts/app/components/alert.component.tsx
+++ src/ts/app/components/alert.component.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classNames from 'classnames';
 
 interface IAlertComponentProps {
   status: string;
@@ -11,17 +12,10 @@ export class AlertComponent extends React.Component<any, any> {
     event.preventDefault();
     this.props.closeAlert();
   }
-  setAlertClasses(): string {
-    const { alert } = this.props;
-    let alertClasses = ['alert', 'alert-dismissable'];
-    alertClasses.push(`alert-${alert.status}`);
-    if (alert.visible) alertClasses.push('alert--visible');
-    return alertClasses.join(' ');
-  }
   render() {
     const { alert } = this.props;
     return (
-      <div className={this.setAlertClasses()}>
+      <div className={classNames('alert', 'alert-dismissable', `alert-${alert.status}`, { 'alert--visible': alert.visible })}>
         <a href='#' className='close' aria-label='close' onClick={(event) => this.closeAlert(event)}>&times;</a>
         <strong className='text-capitalize'>{alert.status}!</strong> <span className='text-capitalize'>{alert.message}.</span>
       </div>
