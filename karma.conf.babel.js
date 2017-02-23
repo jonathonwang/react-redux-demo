60bda5768efc122af024f603c662e7ed67a8666a
Jonathon Kosla <jonathonkosla@gmail.com>
2017-02-22 17:54:27 -0800

    travisci and coverage

--- karma.conf.babel.js
+++ karma.conf.babel.js
@@ -24,17 +24,16 @@ module.exports = function (config) {
     mime: {
       'text/x-typescript': ['ts','tsx']
     },
-    // remapIstanbulReporter: {
-    //   reports: {
-    //     html: 'coverage',
-    //     lcovonly: './coverage/coverage.lcov'
-    //   }
-    // },
+    remapIstanbulReporter: {
+      reports: {
+        lcovonly: './coverage/coverage.lcov'
+      }
+    },
     reporters: [
       'spec',
-      'osx'
+      'osx',
       // 'progress'
-      // 'karma-remap-istanbul'
+      'karma-remap-istanbul'
     ],
     port: 9876,
     colors: true,
